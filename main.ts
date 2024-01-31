c84c7319aa4f175dd2c3ea760f297d013e3d5d52
pero <0711kps@gmail.com>
2024-01-31 23:45:00 +0800

    feat: Add route to handle random restaurants command

--- main.ts
+++ main.ts
@@ -1,31 +1,17 @@
-import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
-import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import data from "./data.json" assert { type: "json" };
+import { Application, Router } from 'https://deno.land/x/oak@v12.6.1/mod.ts'
+//import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+//import data from "./data.json" assert { type: "json" };
 
 const router = new Router();
 router
-  .get("/", (context) => {
-    context.response.body = "Welcome to dinosaur API!";
+  .post('/api/v1/random_restaurants', ctx => {
+    console.log("TODO: handle slack's request")
+    ctx.response.status = 200
   })
-  .get("/api", (context) => {
-    context.response.body = data;
-  })
-  .get("/api/:dinosaur", (context) => {
-    if (context?.params?.dinosaur) {
-      const found = data.find((item) =>
-        item.name.toLowerCase() === context.params.dinosaur.toLowerCase()
-      );
-      if (found) {
-        context.response.body = found;
-      } else {
-        context.response.body = "No dinosaurs found.";
-      }
-    }
-  });
 
 const app = new Application();
-app.use(oakCors()); // Enable CORS for All Routes
+//app.use(oakCors()); // Enable CORS for All Routes
 app.use(router.routes());
-app.use(router.allowedMethods());
+//app.use(router.allowedMethods());
 
 await app.listen({ port: 8000 });
